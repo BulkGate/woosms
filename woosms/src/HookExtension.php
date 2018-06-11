e5d17dbe988a345f85b1ef79643e6f8fc3219fb1
Lukáš Piják <lukaspijak@gmail.com>
2018-06-11 15:37:38 +0200

    hooks extension via wordpress action

--- woosms/src/HookExtension.php
+++ woosms/src/HookExtension.php
@@ -11,17 +11,6 @@ class HookExtension extends Extensions\Strict implements Extensions\Hook\IExtens
 {
     public function extend(Extensions\Database\IDatabase $database, Extensions\Hook\Variables $variables)
     {
-        /** @example PSEUDO CODE
-
-        $result = $database->execute('SELECT `tracking_number` FROM `order` WHERE order_id = "'.$database->escape($variables->get('order_id')).'"');
-
-        if($result->getNumRows())
-        {
-            $row = $result->getRow();
-
-            $variables->set('tracking_number', $row['tracking_number']);
-        }
-
-        */
+        do_action('woosms_extends_variables', $variables, $database);
     }
 }
