719155756ec6ff0f3e84d6740cc4c0a277e86d81
Lukáš Piják <lukaspijak@gmail.com>
2023-06-07 15:26:07 +0200

    languages rework

--- src/Eshop/LanguageWordpress.php
+++ src/Eshop/LanguageWordpress.php
@@ -8,12 +8,44 @@ namespace BulkGate\WooSms\Eshop;
  */
 
 use BulkGate\Plugin\{Eshop\Language};
-use function woosms_load_languages;
+use function get_locale, defined, is_plugin_active, apply_filters;
 
 class LanguageWordpress implements Language
 {
     public function load(): array
     {
-        return woosms_load_languages();
-    }
+	    $output = [];
+
+	    if (is_plugin_active('sitepress-multilingual-cms-master/sitepress.php') || is_plugin_active('sitepress-multilingual-cms/sitepress.php'))
+	    {
+		    $languages = apply_filters('wpml_active_languages', null, 'orderby=id&order=desc');
+
+		    foreach ($languages as $iso => $item)
+		    {
+			    $output[$iso] = $item['native_name'] ?? $iso;
+		    }
+	    }
+		else
+		{
+			$output = [get_locale() => 'Default'];
+		}
+
+		return $output;
+	}
+
+
+	public function get(?int $id = null): string
+	{
+		if (
+			(is_plugin_active('sitepress-multilingual-cms-master/sitepress.php') || is_plugin_active('sitepress-multilingual-cms/sitepress.php')) &&
+			defined('ICL_LANGUAGE_CODE')
+		)
+		{
+			return $id === null ? ICL_LANGUAGE_CODE : ((string) get_post_meta($id, 'wpml_language', true) ?: ICL_LANGUAGE_CODE);
+		}
+		else
+		{
+			return get_locale();
+		}
+	}
 }
